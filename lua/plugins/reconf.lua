4fd5b7a8ddd6c8cb6f46c133745d1ccbf263a3dd
Veha0001 <vehav00@gmail.com>
2024-11-19 06:28:46 +0700

    lazy night

--- lua/plugins/reconf.lua
+++ lua/plugins/reconf.lua
@@ -68,7 +68,6 @@ return {
         -- LSP Server Settings
         ---@type lspconfig.options
         servers = {
-          emmet_ls = {},
           lua_ls = {
             mason = false, -- set to false if you don't want this server to be installed with mason
             -- Use this to add any additional keymaps
@@ -293,5 +292,5 @@ return {
       end)
     end,
   },
-  -- { "williamboman/mason-lspconfig.nvim", config = function() end },
+  { "williamboman/mason-lspconfig.nvim", config = function() end },
 }
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -9,4 +9,5 @@ return {
   },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
 }
